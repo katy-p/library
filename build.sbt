80ec03f1aa03c8c309456101a02c4a9f19ed59e2
Simon Popugaev <seniapsm@gmail.com>
2018-09-30 19:45:26 +0300

    initial commit

---
+++ build.sbt
@@ -0,0 +1,17 @@
+name := "library"
+
+resolvers in ThisBuild += Resolver.jcenterRepo
+
+val springVersion = "5.1.0.RELEASE"
+
+libraryDependencies += "org.springframework" % "spring-webflux" % springVersion
+
+val junitVersion = "5.3.1"
+
+testOptions += Tests.Argument(jupiterTestFramework, "-q", "-v")
+
+libraryDependencies ++= Seq(
+  "org.junit.jupiter" % "junit-jupiter-api" % junitVersion % Test,
+	"org.junit.jupiter" % "junit-jupiter-params" % junitVersion % Test,
+	"org.junit.jupiter" % "junit-jupiter-engine" % junitVersion % Test
+)
---
+++ src/test/java/katy/library/DummyTests.java
@@ -0,0 +1,29 @@
+package katy.library;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class DummyTests {
+
+	@Test
+	@DisplayName("1 + 1 = 2")
+	void addsTwoNumbers() {
+		assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+	}
+
+	@ParameterizedTest(name = "{0} + {1} = {2}")
+	@CsvSource({
+			"0,    1,   1",
+			"1,    2,   3",
+			"49,  51, 100",
+			"1,  100, 101"
+	})
+	void add(int first, int second, int expectedResult) {
+		assertEquals(expectedResult, first + second,
+				() -> first + " + " + second + " should equal " + expectedResult);
+	}
+}
